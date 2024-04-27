274542ee1b423ebbc13d90d0c9639c1660f92577
andyk2000 <125449101+andyk2000@users.noreply.github.com>
2024-04-27 07:32:58 +0300

    Submit update for the cover page

--- src/pages/client/Home.js
+++ src/pages/client/Home.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { Button } from '../../components/Button';
+import './Home.css';
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='board'>
+        <div className='board-left'>
+            <p className='title-cover'>Ask for what you want and be prepared to get it!</p>
+            <p className='author'>By Diane U   wamariya</p>
+            <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>SUBSCRIBE</Button>
+        </div>
+        <div className='board-right'>
+            <p>right</p>
+        </div>
+    </div>
   )
 }
 
-export default Home
+export default Home;
