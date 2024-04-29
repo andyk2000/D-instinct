c1d575b50c0e746d42f6b55b9667f0ac5bf73854
andyk2000 <125449101+andyk2000@users.noreply.github.com>
2024-04-29 14:22:33 +0300

    upload2 footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,32 +1,40 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Footer.css';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
 
 function Footer() {
   return (
     <div className='footer'>
         <div className='footer-part1'>
             <div className='footer-logo'>
-                <Link >D-INSTINCT</Link>
+                <Link className='footer-logo-img'>D-INSTINCT</Link>
             </div>
-            <div className='footer-col1'>
+            <div className='footer-col'>
                 <h1>ABOUT</h1>
-                <Link>BODY</Link>
-                <Link>SPIRIT</Link>
-                <Link>SOUL</Link>
-                <Link>SOCIETY</Link>
+                <Link className='footer-element-1'>BODY</Link>
+                <Link className='footer-element-1'>SPIRIT</Link>
+                <Link className='footer-element-1'>SOUL</Link>
+                <Link className='footer-element-1'>SOCIETY</Link>
             </div>
-            <div className='footer-col2'>
+            <div className='footer-col'>
                 <h1>CONTACT</h1>
-                <Link>diane@gmail.com</Link>
-                <Link>(+250)780000000</Link>
+                <Link className='footer-element-1'>diane@gmail.com</Link>
+                <Link className='footer-element-1'>(+250)780000000</Link>
             </div>
-            <div className='footer-col3'>
+            <div className='footer-col'>
                 <h1>SOCIAL MEDIA</h1>
-                <Link>DianeUwamariya</Link>
-                <Link>DianeUwamariya</Link>
-                <Link>DianeUwamariya</Link>
-                <Link>DianeUwamariya</Link>
+                <Link className='footer-element-1'>
+                    <LinkedInIcon />
+                    DianeUwamariya</Link>
+                <Link className='footer-element-1'>
+                    <InstagramIcon />
+                    DianeUwamariya</Link>
+                <Link className='footer-element-1'>
+                    <FacebookIcon />
+                    DianeUwamariya</Link>
             </div>
         </div>
     </div>
