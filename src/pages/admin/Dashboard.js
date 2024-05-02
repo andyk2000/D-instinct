01935bcc3031368db81830d544334106e5f73f79
andyk2000 <125449101+andyk2000@users.noreply.github.com>
2024-05-02 22:40:37 +0300

    change logo and title of the app

---
+++ src/pages/admin/Dashboard.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import '../../../public/D-instinct logo.png';
+
+function Dashboard() {
+  return (
+    <div className='dashboard'>
+      <div className='side-bar'>
+        <img src="../../../public/D-instinct logo.png" alt="" className='logo'/>
+      </div>
+      <div className='main'>
+        <div className='nav-bar'>
+
+        </div>
+        <div className='main-area'>
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Dashboard
