be67d3cb66463e4639388dec3a06af93353c4575
andyk2000 <125449101+andyk2000@users.noreply.github.com>
2024-05-02 23:25:09 +0300

    admin dashboard side bar update

--- src/pages/admin/Dashboard/Sidebar.js
+++ src/pages/admin/Dashboard/Sidebar.js
@@ -10,31 +10,33 @@ import './Sidebar.css';
 function Sidebar() {
   return (
     <div className='side-bar'>
-        
-        <div className='tab-container' >
-          <div className='home-tab'>
-            <HomeIcon className='home-icon' />
-            <p className='home-label'><Link className='home-txt'>Home</Link></p>
-          </div>
-          <div className='data-tab'>
-            <DataUsageIcon className='data-icon' />
-            <p className='data-label'><Link className='data-txt'>Data</Link></p>
-          </div>
-          <div className='article-tab'>
-            <NewspaperIcon className='article-icon' />
-            <p className='article-label'><Link className='article-txt'>Data</Link></p>
-          </div>
-          <div className='payment-tab'>
-            <PaymentIcon className='payment-icon' />
-            <p className='payment-label'><Link className='payment-txt'>Data</Link></p>
-          </div>
-        </div>
-        <div className='logout-tab'>
-            <LogoutIcon className='logout-icon' />
-            <p className='logout-label'><Link className='logout-txt'>Data</Link></p>
-          </div>
+      <div className='image frame'>
+        <img src=''
       </div>
-  )
+      <div className='tab-container'>
+        <div className='home-tab'>
+          <HomeIcon className='home-icon' />
+          <p className='home-label'><Link to='/home' className='home-txt'>Home</Link></p>
+        </div>
+        <div className='data-tab'>
+          <DataUsageIcon className='data-icon' />
+          <p className='data-label'><Link to='/data' className='data-txt'>Data</Link></p>
+        </div>
+        <div className='article-tab'>
+          <NewspaperIcon className='article-icon' />
+          <p className='article-label'><Link to='/articles' className='article-txt'>Articles</Link></p>
+        </div>
+        <div className='payment-tab'>
+          <PaymentIcon className='payment-icon' />
+          <p className='payment-label'><Link to='/payment' className='payment-txt'>Payment</Link></p>
+        </div>
+      </div>
+      <div className='logout-tab'>
+        <LogoutIcon className='logout-icon' />
+        <p className='logout-label'><Link to='/logout' className='logout-txt'>Logout</Link></p>
+      </div>
+    </div>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
